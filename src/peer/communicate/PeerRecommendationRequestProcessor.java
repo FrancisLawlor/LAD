e299d7e4401db1afcf55f88ac27ad6e8d8331272
Rory Buckley <Rory.Buckley.89@gmail.com>
2017-11-21 14:03:34 +0000

    Initial Apache Camel and REST routing framework

---
+++ src/peer/communicate/PeerRecommendationRequestProcessor.java
@@ -0,0 +1,10 @@
+package peer.communicate;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class PeerRecommendationRequestProcessor implements Processor {
+    public void process(Exchange exchange) {
+        
+    }
+}
