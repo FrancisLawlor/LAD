032f83c4f6594bd3bf4ef03fd54d25febe63f892
Rory Buckley <Rory.Buckley.89@gmail.com>
2017-12-09 14:36:51 +0000

    Retriever filled in a bit more

--- src/content/retrieve/LocalRetrieveContentRequest.java
+++ src/content/retrieve/LocalRetrieveContentRequest.java
@@ -2,9 +2,15 @@ package content.retrieve;
 
 import content.content.Content;
 import core.ActorMessage;
+import core.UniversalId;
 
 public class LocalRetrieveContentRequest extends ActorMessage {
     public LocalRetrieveContentRequest(Content content) {
         
+    }
+    
+    public UniversalId getRecipient() {
+        
+        return null;
     }
 }
--- src/content/retrieve/Retriever.java
+++ src/content/retrieve/Retriever.java
@@ -1,5 +1,6 @@
 package content.retrieve;
 
+import akka.actor.ActorSelection;
 import akka.actor.UntypedActor;
 import core.xcept.UnknownMessageException;
 
@@ -33,18 +34,23 @@ public class Retriever extends UntypedActor {
     }
     
     /**
-     * Viewer delegates to a local Retriever to request Content
+     * Viewer is delegating to its local Retriever to request Content from another peer
+     * This local retriever will use its communicator to request content from this other peer
      * @param request
      */
     protected void processLocalRetrieveContentRequest(LocalRetrieveContentRequest request) {
+        PeerRetrieveContentRequest retrieveRequest = new PeerRetrieveContentRequest(request);
         
+        ActorSelection communicator = getContext().actorSelection("user/communicator");
+        communicator.tell(retrieveRequest, getSelf());
     }
     
     /**
      * This peer's retriever is being asked by another peer to find the content
      */
     protected void processPeerRetrieveContentRequest(PeerRetrieveContentRequest request) {
-        
+        // Find content locally
+        // If it has been deleted locally then...
     }
     
     /**
@@ -52,6 +58,7 @@ public class Retriever extends UntypedActor {
      * @param retrievedContent
      */
     protected void processRetrievedContent(RetrievedContent retrievedContent) {
-        
+        ActorSelection viewer = getContext().actorSelection("user/viewer");
+        viewer.tell(retrievedContent, getSelf());
     }
 }
