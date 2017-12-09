a43e0b41aed7bac51aa3a19f8270452a190a451e
Rory Buckley <Rory.Buckley.89@gmail.com>
2017-12-09 14:39:21 +0000

    Content sketched out

--- src/content/content/Content.java
+++ src/content/content/Content.java
@@ -1,6 +1,16 @@
 package content.content;
 
+/**
+ * Contains information about a piece of content
+ * Content Id is a hash of the digital file to uniquely identify it
+ *
+ */
 public class Content {
+    private String uniqueId;
+    
+    public String getId() {
+        return this.uniqueId;
+    }
     
     public int getViewLength() {
         return 0;
