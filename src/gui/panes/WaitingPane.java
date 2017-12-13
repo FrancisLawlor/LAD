5501aa30798f3e71c85c8d57ee0e352f331b4516
Francis Lawlor <franlawlor0@gmail.com>
2017-12-13 21:45:01 +0000

    Added Waiting Pane and scenes for file retrieval and finding recommendations.

---
+++ src/gui/panes/WaitingPane.java
@@ -0,0 +1,25 @@
+package gui.panes;
+
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+public class WaitingPane extends BorderPane {
+	public WaitingPane(String waitingMessage) {
+		StackPane waitingContent = configureStackPane(waitingMessage);
+		
+		this.setCenter(waitingContent);
+	}
+
+	private StackPane configureStackPane(String waitingMessage) {
+		StackPane content = new StackPane();
+		ProgressIndicator progressIndicator = new ProgressIndicator();
+		progressIndicator.setMaxSize(150, 150);
+			    	
+	    	content.getChildren().add(progressIndicator);
+	    	content.getChildren().add(new Text(waitingMessage));
+	    	
+	    	return content;
+	}
+}
