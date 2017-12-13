9479cfcc88028363cb296e17e7b1c5cb2820c6c3
Francis Lawlor <franlawlor0@gmail.com>
2017-12-13 17:38:33 +0000

    Added string constants for GUI.

---
+++ src/gui/utilities/GUIText.java
@@ -0,0 +1,17 @@
+package gui.utilities;
+public class GUIText {
+	public static final String FINDING_RECOMMENDATIONS = "Finding recommendations...";
+	public static final String RETRIEVING_FILE = "Retrieving file...";
+	public static final String PORT_NUMBER = "Port number:";
+	public static final String SETUP = "Setup";
+	public static final String BUTTON_NEXT = "Next";
+	public static final String MY_FILES = "My files";
+	public static final String ADD_FILE = "Add file";
+	public static final String REFRESH = "Refresh";
+	public static final String CONFIRMATION_FILE_RETRIEVAL = "Are you sure you want to retrieve this file?";
+	public static final String CONFIRMATION_NO = "No";
+	public static final String CONFIRMATION_YES = "Yes";
+	public static final String BACK_BUTTON = "Back";
+	public static final String RATE = "Rate:";
+	public static final String SUBMIT = "Submit";
+}
