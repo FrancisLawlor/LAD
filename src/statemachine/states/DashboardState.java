6dd3814567467116945a7316eb585604ccbf22dd
Francis Lawlor <franlawlor0@gmail.com>
2017-12-14 18:05:23 +0000

    Updated Dashboard and Add file states.

--- src/statemachine/states/DashboardState.java
+++ src/statemachine/states/DashboardState.java
@@ -1,5 +1,11 @@
 package statemachine.states;
 
-public class DashboardState {
+public class DashboardState implements IState {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
