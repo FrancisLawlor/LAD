af78418db9a76c99df95e513f2f6565e3b0be181
Rory Buckley <Rory.Buckley.89@gmail.com>
2017-12-23 12:49:42 +0000

    Contenter finished

--- src/content/contenter/Contenter.java
+++ src/content/contenter/Contenter.java
@@ -1,8 +1,18 @@
 package content.contenter;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import akka.actor.ActorSelection;
+import core.ActorPaths;
 import core.PeerToPeerActor;
 import core.PeerToPeerActorInit;
+import core.UniversalId;
+import peer.graph.link.PeerLinkAddition;
+import peer.graph.link.PeerLinkExistenceRequest;
 import peer.graph.link.PeerLinkExistenceResponse;
+import peer.graph.weight.LocalWeightUpdateRequest;
+import peer.graph.weight.Weight;
 
 /**
  * Updates theoretical graph based on Header data from Retrieved Content
@@ -10,6 +20,11 @@ import peer.graph.link.PeerLinkExistenceResponse;
  *
  */
 public class Contenter extends PeerToPeerActor {
+    private Map<UniversalId, Weight> weights;
+    
+    public Contenter() {
+        this.weights = new HashMap<UniversalId, Weight>();
+    }
     
     /**
      * Actor message processing
@@ -30,11 +45,43 @@ public class Contenter extends PeerToPeerActor {
         }
     }
     
-    protected void processPeerSimilarViewAlert(PeerSimilarViewAlert notify) {
+    /**
+     * Temporarily stores this peer's similarity weight for this similar view
+     * Asks if peerLinker for the theoretical peer weight graph already has a link or not
+     * How to add this weight to the graph depends on the existence or nonexistence of the link
+     * 
+     * @param alert
+     */
+    protected void processPeerSimilarViewAlert(PeerSimilarViewAlert alert) {
+        UniversalId similarPeerId = alert.getSimilarViewPeerId();
+        Weight weightToGive = alert.getWeightToGive();
+        this.weights.put(similarPeerId, weightToGive);
         
+        PeerLinkExistenceRequest request = new PeerLinkExistenceRequest(similarPeerId);
+        ActorSelection peerLinker = getContext().actorSelection(ActorPaths.getPathToPeerLinker());
+        peerLinker.tell(request, getSelf());
     }
     
+    /**
+     * Updates the weight if the peer is in existence
+     * Otherwise requests a new link with this starting weight be made
+     * @param response
+     */
     protected void processPeerLinkExistenceResponse(PeerLinkExistenceResponse response) {
-        
+        UniversalId peerId = response.getLinkToCheckPeerId();
+        boolean linkExists = response.isLinkInExistence();
+        if (this.weights.containsKey(peerId)) {
+            Weight weight = this.weights.get(peerId);
+            if (linkExists) {
+                LocalWeightUpdateRequest weightUpdate = new LocalWeightUpdateRequest(peerId, weight);
+                ActorSelection weighter = getContext().actorSelection(ActorPaths.getPathToWeighter(peerId));
+                weighter.tell(weightUpdate, getSelf());
+            }
+            else {
+                PeerLinkAddition linkAddition = new PeerLinkAddition(peerId, weight);
+                ActorSelection peerLinker = getContext().actorSelection(ActorPaths.getPathToPeerLinker());
+                peerLinker.tell(linkAddition, getSelf());
+            }
+        }
     }
 }
--- src/content/contenter/PeerSimilarViewAlert.java
+++ src/content/contenter/PeerSimilarViewAlert.java
@@ -3,6 +3,7 @@ package content.contenter;
 import core.ActorMessage;
 import core.ActorMessageType;
 import core.UniversalId;
+import peer.graph.weight.Weight;
 
 /**
  * Alerts Contenter that a peer has viewed similar content
@@ -10,12 +11,19 @@ import core.UniversalId;
  */
 public class PeerSimilarViewAlert extends ActorMessage {
     private UniversalId similarViewPeerId;
+    private Weight weightToGive;
     
-    public PeerSimilarViewAlert() {
+    public PeerSimilarViewAlert(UniversalId similarViewPeerId, Weight weightToGive) {
         super(ActorMessageType.PeerSimilarViewAlert);
+        this.similarViewPeerId = similarViewPeerId;
+        this.weightToGive = weightToGive;
     }
     
     public UniversalId getSimilarViewPeerId() {
         return this.similarViewPeerId;
+    }
+    
+    public Weight getWeightToGive() {
+        return this.weightToGive;
     }
 }
