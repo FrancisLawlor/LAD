4fb81d3a34f8247bbb9552d73551be49bad2148d
13366801 <eoin.kerr@ucdconnect.ie>
2018-01-04 16:29:00 +0000

    added hashing util & authenticator

---
+++ src/crypto/HashUtil.java
@@ -0,0 +1,54 @@
+package crypto;
+
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/*
+    Using Daniel J. Bernsteins hash function dqjb
+    extremely efficient
+ */
+public class HashUtil {
+
+    public static String generateHash(byte bytestream[]){
+        long hash = 5381;
+        int a = 33;
+        for(int i = 0; i < bytestream.length; ++i){
+            hash = ((hash << 5) + hash) + bytestream[i];
+        }
+        return Long.toUnsignedString(hash);
+    }
+    /*
+        public static String generateHash(Scanner file){
+            long hash = 5381;
+            int a = 33;
+            while(file.next()){
+                hash = ((hash << 5) + hash) + file[i];
+            }
+            return Long.toUnsignedString(hash);
+        }*/
+    //compares a hash with a files hash
+    public static Boolean authenticateHash(byte bytestream[], String hash){
+        return (generateHash(bytestream).equals(hash));
+    }
+    /*
+        for testing
+    */
+    public static void main(String Args[]){
+        long j = 2;
+        while(j<(long)Math.pow(Math.pow(1048576,2),2)){
+            byte bstream[] = new byte[(int)j-2];
+            for(int i =0; i<j-2 ;i++) {
+                bstream[i] = (byte) (Math.random() * 127);
+            }
+            System.out.print(j + "  ");
+            System.out.println(generateHash(bstream));
+            String hash = generateHash(bstream);
+            System.out.println(authenticateHash(bstream,hash));
+            j *= 2;
+        }
+
+    }
+}
