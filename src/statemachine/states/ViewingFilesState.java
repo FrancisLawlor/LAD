f5f2fb1735ee51f56d45ade32b517f3951af842f
Francis Lawlor <franlawlor0@gmail.com>
2018-01-13 23:47:17 +0000

    Implemented file loading.

--- src/statemachine/states/ViewingFilesState.java
+++ src/statemachine/states/ViewingFilesState.java
@@ -1,6 +1,13 @@
 package statemachine.states;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
 import content.frame.core.Content;
+import filemanagement.core.FileConstants;
+import filemanagement.fileretrieval.MediaFileSaver;
+import filemanagement.fileretrieval.RetrievedFile;
 import gui.core.GUI;
 import gui.core.SceneContainerStage;
 import gui.utilities.GUIText;
@@ -8,6 +15,7 @@ import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.ListView;
+import peer.data.messages.LoadedContent;
 import peer.data.messages.LocalSavedContentResponse;
 import peer.frame.core.ViewerToUIChannel;
 import statemachine.core.StateMachine;
@@ -88,10 +96,55 @@ public class ViewingFilesState extends State {
 	}
 	
 	private void clicksFile() {
-		// TODO
-		// file opens
-		// change to rating state
-		// change to rating scene
+		RetrievedFile retrievedFile = new RetrievedFile();
+
+		Task<Void> task = new Task<Void>() {
+			@Override
+			public Void call() throws Exception {
+			    LoadedContent loadedContent;
+			    Content content = gui.getMyFilesScene().getFilesListView().getSelectionModel().getSelectedItem();
+			    viewer.requestRetrievalOfSavedContentFile(content);
+			    loadedContent = viewer.getLoadedContent();
+			    viewer.createNewContentView(loadedContent.getContent());
+			    String fileName = loadedContent.getContent().getFileName();
+			    String fileFormat = loadedContent.getContent().getFileFormat();
+			    File file = MediaFileSaver.getFile(fileName, fileFormat);
+			    retrievedFile.setFile(file);
+				return null;
+			}
+		};
+		task.setOnSucceeded(e -> {
+			try {
+				openFile(retrievedFile);
+				rateFile();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				}
+		});
+		new Thread(task).start();
+	}
+	
+	private void openFile(RetrievedFile retrievedFile) throws IOException {
+	    if (Desktop.isDesktopSupported()) {
+            if (retrievedFile.getFile().exists()) {
+                new Thread(() -> {
+                    try {
+                        Desktop desktop = Desktop.getDesktop();
+                        desktop.open(retrievedFile.getFile()); 
+                    }
+                    catch (IOException e) { }
+                }).start();
+            }
+	    }
+	    else {
+			System.err.println(FileConstants.DESKTOP_NOT_SUPPORTED);
+			return;
+		}
+	}
+	
+	private void rateFile() {
+		stateMachine.setCurrentState(StateName.RATING.toString());
+		stateMachine.execute(StateName.INIT);
 	}
 
 }
