1da66a27c8b5097fcb54dd92728e43ee1637a171
Rory Buckley <Rory.Buckley.89@gmail.com>
2018-01-15 01:05:32 +0000

    DistributedRecommenderSystem fully tested and deployed

--- src/tests/system/SystemTestApplicationWindow.java
+++ src/tests/system/SystemTestApplicationWindow.java
@@ -7,6 +7,7 @@ import statemachine.utils.StateName;
 
 public class SystemTestApplicationWindow extends Application {
     private static PeerOne peerOne;
+    private static Thread[] THREADS;
     
     public void start(Stage stage) {
             StateMachine stateMachine = new StateMachine(peerOne);
@@ -17,6 +18,21 @@ public class SystemTestApplicationWindow extends Application {
     }
     
     public static void main(String[] args) throws Exception {
+        THREADS = new Thread[9];
+        THREADS[0] = new Thread(new PeerTwo());
+        THREADS[1] = new Thread(new PeerThree());
+        THREADS[2] = new Thread(new PeerFour());
+        THREADS[3] = new Thread(new PeerFive());
+        THREADS[4] = new Thread(new PeerSix());
+        THREADS[5] = new Thread(new PeerSeven());
+        THREADS[6] = new Thread(new PeerEight());
+        THREADS[7] = new Thread(new PeerNine());
+        THREADS[8] = new Thread(new PeerTen());
+        
+        for (Thread thread : THREADS) {
+            thread.start();
+        }
+        
         peerOne = new PeerOne();
         
         launch(args);
